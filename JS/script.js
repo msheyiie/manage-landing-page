72593b296affb096d636cf66fc3fd28c652ef7da
seyi <seyi@gmail.com>
2020-05-02 13:10:11 +0100

    Initial commit

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+
+function changeBackgroundColour(){
+    document.getElementById("changeBody").style.backgroundColor = "hsla(0, 15%, 93%, 0.67)";
+}
+
+function changeBackgroundBack(){
+    document.getElementById("changeBody").style.backgroundColor =  "hsl(0, 0%, 98%)";
+}
